62243ae7fcf872555a634e205fc9cd27e07d88c2
Timor <timorrigter@gmail.com>
2026-06-23 14:53:17 +0000

    Added some things

--- app.ts
+++ app.ts
@@ -3,42 +3,97 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const STREAM_KEY = process.env.TWITCH_STREAM_KEY;
+
 if (!STREAM_KEY) {
-    throw new Error("TWITCH_STREAM_KEY ontbreekt in .env");
+  throw new Error("TWITCH_STREAM_KEY ontbreekt in .env");
 }
 
-const STREAM_KEY = process.env.TWITCH_STREAM_KEY;
 const RTMP_URL = `rtmp://live.twitch.tv/app/${STREAM_KEY}`;
 
-const ffmpeg = spawn("ffmpeg", [
-    "-loop", "1",
-    "-i", "image.png",
+if (process.env.STREAM === "image") {
+  console.log("Streaming image...");
 
-    "-f", "lavfi",
-    "-i", "anullsrc=channel_layout=stereo:sample_rate=44100",
+  // Image
+  const ffmpeg = spawn("ffmpeg", [
+    "-loop",
+    "1",
+    "-i",
+    "data/images/private/image.png",
 
-    "-c:v", "libx264",
-    "-preset", "veryfast",
-    "-tune", "stillimage",
+    "-f",
+    "lavfi",
+    "-i",
+    "anullsrc=channel_layout=stereo:sample_rate=44100",
 
-    "-pix_fmt", "yuv420p",
-    "-r", "30",
+    "-c:v",
+    "libx264",
+    "-preset",
+    "veryfast",
+    "-tune",
+    "stillimage",
 
-    "-c:a", "aac",
-    "-b:a", "128k",
+    "-pix_fmt",
+    "yuv420p",
+    "-r",
+    "30",
 
-    "-g", "60",
+    "-c:a",
+    "aac",
+    "-b:a",
+    "128k",
+
+    "-g",
+    "60",
 
     "-shortest",
 
-    "-f", "flv",
-    RTMP_URL
-]);
+    "-f",
+    "flv",
+    RTMP_URL,
+  ]);
 
-ffmpeg.stderr.on("data", (data) => {
+  ffmpeg.stderr.on("data", (data) => {
     console.log(data.toString());
-});
+  });
 
-ffmpeg.on("close", (code) => {
+  ffmpeg.on("close", (code) => {
     console.log(`FFmpeg afgesloten met code ${code}`);
-});
+  });
+}
+
+if (process.env.STREAM === "video") {
+  console.log("Streaming video...");
+
+  // Video
+  const ffmpeg = spawn("ffmpeg", [
+    "-re", // speel af op normale snelheid
+    "-i",
+    "data/images/private/video.mp4",
+
+    "-c:v",
+    "libx264",
+    "-preset",
+    "veryfast",
+
+    "-c:a",
+    "aac",
+    "-b:a",
+    "128k",
+
+    "-pix_fmt",
+    "yuv420p",
+
+    "-f",
+    "flv",
+    RTMP_URL,
+  ]);
+
+  ffmpeg.stderr.on("data", (data) => {
+    console.log(data.toString());
+  });
+
+  ffmpeg.on("close", (code) => {
+    console.log(`FFmpeg afgesloten met code ${code}`);
+  });
+}
